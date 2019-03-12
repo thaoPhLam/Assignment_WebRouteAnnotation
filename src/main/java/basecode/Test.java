ea087872c005d74dc8aa6ac7ea04d51a8f498f14
Thao Phuong Lam <lphuongt930906@gmail.com>
2019-03-12 20:04:55 +0100

    added a comment

--- src/main/java/basecode/Test.java
+++ src/main/java/basecode/Test.java
@@ -12,8 +12,10 @@ public class Test {
     public static void main(String[] args) throws Exception {
         HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
 
-        //server.createContext("/test", new MyHandler());
-        //server.createContext("/another", new AnotherMyHandler());
+/*
+        server.createContext("/test", new MyHandler());
+        server.createContext("/another", new AnotherMyHandler());
+ */
 
         /**
          * itt kezdődik az assignment
