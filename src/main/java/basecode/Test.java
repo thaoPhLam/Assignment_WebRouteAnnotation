a9e9944a069cf549508bcbf61f09e4168e6702eb
Thao Phuong Lam <lphuongt930906@gmail.com>
2019-03-14 12:27:47 +0100

    deleted comments

--- src/main/java/basecode/Test.java
+++ src/main/java/basecode/Test.java
@@ -12,11 +12,6 @@ public class Test {
     public static void main(String[] args) throws Exception {
         HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
 
-/*
-        server.createContext("/test", new MyHandler());
-        server.createContext("/another", new AnotherMyHandler());
-*/
-
         /**
          * itt kezdődik az assignment
          *
@@ -26,12 +21,7 @@ public class Test {
             if (method.isAnnotationPresent(WebRoute.class)) {
                 Annotation annotation = method.getAnnotation(WebRoute.class);
                 WebRoute webRoute = (WebRoute) annotation;
-                /*
-                Here comes your logic.
-                If the given path from the HttpExchange method
-                is the SAME like the WebRoute annotation's path,
-                you should INVOKE this method.
-                */
+
                 server.createContext(webRoute.path(), new Routes());
 
             }
@@ -41,27 +31,4 @@ public class Test {
         server.start();
 
     }
-/*
-    static class MyHandler implements HttpHandler {
-
-        public void handle(HttpExchange t) throws IOException {
-            String response = "This is the response";
-            t.sendResponseHeaders(200, response.length());
-            OutputStream os = t.getResponseBody();
-            os.write(response.getBytes());
-            os.close();
-        }
-    }
-
-    static class AnotherMyHandler implements HttpHandler {
-
-        public void handle(HttpExchange httpExchange) throws IOException {
-            String response = "This is ANOTHER response";
-            httpExchange.sendResponseHeaders(200, response.length());
-            OutputStream os = httpExchange.getResponseBody();
-            os.write(response.getBytes());
-            os.close();
-        }
-    }
-*/
 }
