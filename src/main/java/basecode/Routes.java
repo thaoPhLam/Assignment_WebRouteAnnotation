aaf0ed853ebdeaf4903ef145b8b0c64515fa0778
Thao Phuong Lam <lphuongt930906@gmail.com>
2019-03-15 12:45:36 +0100

    rewrote class

--- src/main/java/basecode/Routes.java
+++ src/main/java/basecode/Routes.java
@@ -1,31 +1,18 @@
 package basecode;
 
 import annotation.WebRoute;
-import com.sun.net.httpserver.HttpExchange;
-import com.sun.net.httpserver.HttpHandler;
 
-import java.io.IOException;
-import java.io.OutputStream;
-
-public class Routes implements HttpHandler {
+public class Routes {
     @WebRoute(path = "/test")
-    public String test1(HttpExchange t) throws IOException {
+    public String test1() {
         String response = "This is the response";
-        t.sendResponseHeaders(200, response.length());
-        OutputStream os = t.getResponseBody();
-        os.write(response.getBytes());
-        os.close();
         return response;
     }
 /*
 TODO: csináljak külön osztályt az egyes route-oknak?
     @WebRoute(path = "/another")
-    public String test2(HttpExchange t) throws IOException {
+    public String test2() {
         String response = "This is ANOTHER response";
-        t.sendResponseHeaders(200, response.length());
-        OutputStream os = t.getResponseBody();
-        os.write(response.getBytes());
-        os.close();
         return response;
     }
 */
@@ -42,8 +29,4 @@ TODO: Extra features
 
     }
 */
-    public void handle(HttpExchange httpExchange) throws IOException {
-        test1(httpExchange);
-        //test2(httpExchange);
-    }
 }
