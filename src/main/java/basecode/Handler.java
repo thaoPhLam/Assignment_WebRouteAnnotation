38cb6060391ab568f912ead35fd6010c3a355876
Thao Phuong Lam <lphuongt930906@gmail.com>
2019-03-15 12:50:14 +0100

    added a field & constructor

--- src/main/java/basecode/Handler.java
+++ src/main/java/basecode/Handler.java
@@ -1,6 +1,5 @@
 package basecode;
 
-import annotation.WebRoute;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
@@ -8,9 +7,13 @@ import java.io.IOException;
 import java.io.OutputStream;
 
 public class Handler implements HttpHandler {
-    @WebRoute(path = "/another")
+    private String response;
+
+    public Handler(String response) {
+        this.response = response;
+    }
+
     public void handle(HttpExchange t) throws IOException {
-        String response = "This is the response";
         t.sendResponseHeaders(200, response.length());
         OutputStream os = t.getResponseBody();
         os.write(response.getBytes());
